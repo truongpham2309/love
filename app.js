9a6bbc80c1f07dcf798cecd9747badfaa354eae4
truongpham2309 <phamnhattruong2309@gmail.com>
2024-04-12 21:53:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ music = ['ido', 'noinaycoanh', 'nguoiamphu'];
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
 
-      document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
-      
-      document.querySelector("date").textContent = Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24)+" DAYS";
+      document.querySelector("anni").textContent = `${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${yourDate.getFullYear()}`;
+
+		document.querySelector("date").textContent = Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24)+" DAYS";
 
       function olock() {
             var today = new Date(),
